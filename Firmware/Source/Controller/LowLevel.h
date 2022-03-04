c17ea0f73054f1cbb18d3d10039c2d7f8377e8be
d.ivanov <d.ivanov@proton-electrotex.com>
2022-03-04 12:43:25 +0300

    Доработаны функции нижнего уровня

--- Firmware/Source/Controller/LowLevel.h
+++ Firmware/Source/Controller/LowLevel.h
@@ -11,22 +11,8 @@
 // Functions
 //
 void LL_ToggleBoardLED();
-void LL_SetVoltageRange(bool State);
-void LL_SetStateExtMsrLed(bool State);
-void LL_SetStateLineSync1(bool State);
-void LL_SetStateLineSync2(bool State);
-bool LL_GetStateLineSync1();
-bool LL_GetStateLineSync2();
-void LL_PowerSupplyEnable(bool State);
-void LL_WriteDACx(Int16U Data, Int16U CellStartNumber, Int16U CellTotal, bool ToggleLDAC);
-void LL_SetStateCS_SYNC1(bool State);
-void LL_SetStateCS_SYNC2(bool State);
-void LL_SetStateCS_SYNC3(bool State);
-void LL_SetStateCS_SYNC4(bool State);
-void LL_SetStateCS_SYNC5(bool State);
-void LL_SetStateCS_SYNC6(bool State);
-void LL_SetStateCS_SYNC7(bool State);
-void LL_SetStateCS_SYNC8(bool State);
+void LL_SetStateLineSync(bool State);
+void LL_WriteDACx(Int16U Data);
 void LL_SetCurrentRange0();
 void LL_SetCurrentRange1();
 void LL_SetCurrentRange2();
